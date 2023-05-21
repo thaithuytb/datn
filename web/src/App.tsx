565da8021b38679a7d4fba9a01bbb65657850c45
thaithuytb <ngoquangthai29112001@gmail.com>
2023-05-21 22:39:23 +0700

    login form

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -9,41 +9,43 @@ import PersonalInformation from "./pages/PersonalInformation";
 import { ProtectedRoute } from "./routes/protectedRoute";
 import { AdminRoute } from "./routes/adminRoute";
 import NotFound from "./components/NotFound";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <HeaderLayout />
-      <div className="app_content">
-        <SidebarLayout />
-        <Routes>
-          <Route
-            path="/garden/*"
-            element={<ProtectedRoute componentRedirect={Garden} />}
-          />
-          <Route
-            path="/management-worker/*"
-            element={<AdminRoute componentRedirect={NotFound} />}
-          />
-          <Route
-            path="/management-device/*"
-            element={<AdminRoute componentRedirect={NotFound} />}
-          />
-          <Route
-            path="/personal-information"
-            element={<ProtectedRoute componentRedirect={PersonalInformation} />}
-          />
-          <Route
-            path="/change-password"
-            element={<ProtectedRoute componentRedirect={ChangePassword} />}
-          />
-          <Route
-            path="/logout"
-            element={<ProtectedRoute componentRedirect={Logout} />}
-          />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Login />
+    // <BrowserRouter>
+    //   <HeaderLayout />
+    //   <div className="app_content">
+    //     <SidebarLayout />
+    //     <Routes>
+    //       <Route
+    //         path="/garden/*"
+    //         element={<ProtectedRoute componentRedirect={Garden} />}
+    //       />
+    //       <Route
+    //         path="/management-worker/*"
+    //         element={<AdminRoute componentRedirect={NotFound} />}
+    //       />
+    //       <Route
+    //         path="/management-device/*"
+    //         element={<AdminRoute componentRedirect={NotFound} />}
+    //       />
+    //       <Route
+    //         path="/personal-information"
+    //         element={<ProtectedRoute componentRedirect={PersonalInformation} />}
+    //       />
+    //       <Route
+    //         path="/change-password"
+    //         element={<ProtectedRoute componentRedirect={ChangePassword} />}
+    //       />
+    //       <Route
+    //         path="/logout"
+    //         element={<ProtectedRoute componentRedirect={Logout} />}
+    //       />
+    //     </Routes>
+    //   </div>
+    // </BrowserRouter>
   );
 };
 
--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,3 +1,39 @@
+import { useState } from "react";
+import "./index.css";
+
 export default function Login() {
-  return <div>Login</div>;
+  const [data, setData] = useState<any>();
+  const setPrams = (e: any) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
+  const submit = (e: any) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="wp_login">
+      <h1 className="login_title">Đăng nhập</h1>
+      <div className="login">
+        <form onSubmit={submit} className="login_form">
+          <input
+            type="text"
+            placeholder="usename"
+            name="phone"
+            onChange={setPrams}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            onChange={setPrams}
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
 }
