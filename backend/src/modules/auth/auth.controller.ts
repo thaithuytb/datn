afd55b1321b89cedeac651e3a6b10302dee6ea25
thaithuytb <ngoquangthai29112001@gmail.com>
2023-05-22 17:03:06 +0700

    login

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Patch, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { LoginType, UserResponseDetailType } from './models/auth.model';
 import { RegisterDto } from './dto/register.dto';
 import { RoleAdminGuard } from '../../guards/roleAdminGuard';
 import { UpdateInformationDto } from './dto/auth.dto';
+import { responseSuccess } from '../../common/responseSuccess';
 
 @Controller('api/v1/auth')
 export class AuthController {
@@ -13,6 +14,19 @@ export class AuthController {
   @Post('/login')
   async login(@Body('dto') dto: LoginDto): Promise<LoginType> {
     return this.authService.login(dto);
+  }
+
+  @Get('/verify-token')
+  async verifyToken(@Req() req: any): Promise<LoginType> {
+    const authHeader = req.headers.authorization;
+    const token =
+      authHeader &&
+      authHeader.split(' ')[0] === 'Bearer' &&
+      authHeader.split(' ')[1];
+    return responseSuccess(200, {
+      user: req.user,
+      token: token,
+    });
   }
 
   @UseGuards(RoleAdminGuard)
