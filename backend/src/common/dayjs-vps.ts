6334e0fb3c2b45e2aabb939cf1fddc5bb4b35f61
thaithuytb <ngoquangthai29112001@gmail.com>
2023-07-26 23:22:38 +0700

    fix: time

--- backend/src/common/dayjs-vps.ts
+++ backend/src/common/dayjs-vps.ts
@@ -1,5 +1,6 @@
 import * as dayjs from 'dayjs';
+const VIETNAM_ZONE = 7;
 export const dayjsVps = (date: dayjs.Dayjs) => {
-  const substract = process.env.IS_PRODUCTION ? 7 : 0;
-  return dayjs(date).subtract(substract, 'hour');
+  const subtract = process.env.IS_PRODUCTION ? VIETNAM_ZONE : 0;
+  return dayjs(date).subtract(subtract, 'hour');
 };
